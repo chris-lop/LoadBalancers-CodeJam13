5fc7165e87ff7e110b4450624939145fb2f52e97
AriA700 <ari0071@hotmail.com>
2023-11-18 13:59:53 -0500

    Initial landing page
    Co-authored-by: edwardhab <edwardhab@users.noreply.github.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import Dropdown from 'primevue/dropdown'
+import Menubar from 'primevue/menubar'
+import Card from 'primevue/card'
+import Divider from 'primevue/divider'
+import 'primevue/resources/themes/saga-blue/theme.css' // or any theme of your choice
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +21,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+
+app.component('Button', Button)
+app.component('Dropdown', Dropdown)
+app.component('Menubar', Menubar)
+app.component('Card', Card)
+app.component('Divider', Divider)
 
 app.mount('#app')
