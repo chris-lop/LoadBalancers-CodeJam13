fbda1a7aee1485a61ddbd23ef9e2890e9ad83fe3
AriA700 <ari0071@hotmail.com>
2023-11-18 01:33:41 -0500

    Vue + tailwind

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-3xl">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
