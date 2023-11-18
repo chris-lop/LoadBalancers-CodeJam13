0241a4aba87e842d2df5d106a5d0c57c4250f1a6
Chris Lopez <christopherlop12@gmail.com>
2023-11-18 11:52:20 -0500

    Heuristic + Data Filtering
    Co-Authored-By: edwardhab <78373091+edwardhab@users.noreply.github.com>

--- backend/MQTT.py
+++ backend/MQTT.py
@@ -16,9 +16,9 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("CodeJam")
 
 def on_message(client, userdata, msg):
-    # send truck events to truck function
+#     # send truck events to truck function
     payload = json.loads(msg.payload.decode())
-    #print(payload)
+    print(payload)
     if(payload["type"] == "Truck"):
         init_truck(payload)
     elif(payload["type"] == "Load"):
@@ -37,35 +37,45 @@ def init_load(payload):
     load_id = payload["loadId"]
     # {'seq': 51, 'type': 'Load', 'timestamp': '2023-11-17T08:55:55', 'loadId': 40022, 'originLatitude': 29.9561, 'originLongitude': -90.0773, 'destinationLatitude': 33.6821, 'destinationLongitude': -84.1488, 'equipmentType': 'Flatbed', 'price': 1000.0, 'mileage': 480.0}
     if(load_id not in loads):
-        loads[load_id] = {"seq": payload["seq"], "timestamp": payload["timestamp"], "originLatitude": payload["originLatitude"], "originLongitude": payload["originLongitude"], "destinationLatitude": payload["destinationLatitude"], "destinationLongitude": payload["destinationLongitude"], "equipmentType": payload["equipmentType"], "price": payload["price"], "mileage": payload["mileage"]}
+        loads[load_id] = {"seq": payload["seq"], "timestamp": payload["timestamp"], "originLatitude": payload["originLatitude"], "originLongitude": payload["originLongitude"], "destinationLatitude": payload["destinationLatitude"], "destinationLongitude": payload["destinationLongitude"], "equipmentType": payload["equipmentType"], "price": payload["price"], "mileage": payload["mileage"], "potentialTrucks":[]}
     dists = []
     for truck_id, truck in trucks.items():
+        #Only push in heap compatible trucks (size) -> don't bother calculating the rest
+        if truck['equipType'] != payload['equipmentType']:
+            continue
         distance = bird_fly_distance(truck['positionLatitude'], truck["positionLongitude"], payload['originLatitude'], payload["originLongitude"])
         heapq.heappush(dists, (distance, truck_id))    
-    # print 5 closest trucks
-    for i in range(5):
+    # print 50 closest trucks
+    for i in range(50):
         if len(dists) == 0:
             break
         truck_id = heapq.heappop(dists)[1]
-        score = calculate_score(trucks[truck_id], loads[load_id])
-        print(f"Truck {truck_id} has a score of {score}")
-    end_time = time.time()  # end timer
-    elapsed_time = end_time - start_time
-    
-    #print("end init_load in " + str(elapsed_time) + " seconds")
+        loads[load_id]["potentialTrucks"].append(trucks[truck_id])
+        i+=1
+    print("MODIFIED LOAD/n")
+    print(loads[load_id]["potentialTrucks"])
+    print("END OF PRINT/n")
+    #Add to the load a list of all potential trucks
 
-def calculate_score(truck, load):
-    score = 0
-    # Add to the score if the equipment types match
-    if truck['equipType'] == load['equipmentType']:
-        score += 1
-    # Subtract from the score based on the distance between the truck and the load
-    score -= calculate_distance(truck['positionLatitude'], truck["positionLongitude"], load['originLatitude'], load["originLongitude"]) / 1000
-    # Add to the score if the load's mileage fits within the truck's trip length preference
-    if (truck['nextTripLengthPreference'] == 'Short' and load['mileage'] < 200) or \
-       (truck['nextTripLengthPreference'] == 'Long' and load['mileage'] >= 200):
-        score += 1
-    return score
+    #     score = calculate_score(trucks[truck_id], loads[load_id])
+    #     print(f"Truck {truck_id} has a score of {score}")
+    # end_time = time.time()  # end timer
+    # elapsed_time = end_time - start_time
+    
+#     #print("end init_load in " + str(elapsed_time) + " seconds")
+
+# def calculate_score(truck, load):
+#     score = 0
+#     # Add to the score if the equipment types match
+#     if truck['equipType'] == load['equipmentType']:
+#         score += 1
+#     # Subtract from the score based on the distance between the truck and the load
+#     score -= calculate_distance(truck['positionLatitude'], truck["positionLongitude"], load['originLatitude'], load["originLongitude"]) / 1000
+#     # Add to the score if the load's mileage fits within the truck's trip length preference
+#     if (truck['nextTripLengthPreference'] == 'Short' and load['mileage'] < 200) or \
+#        (truck['nextTripLengthPreference'] == 'Long' and load['mileage'] >= 200):
+#         score += 1
+#     return score
 
 def bird_fly_distance(truck_lat, truck_long, load_lat, load_long):
     # Radius of the Earth in meters
@@ -87,25 +97,33 @@ def bird_fly_distance(truck_lat, truck_long, load_lat, load_long):
 
     return distance
 
-def calculate_distance(truck_lat, truck_long, load_lat, load_long):
-    api_key = os.getenv('GOOGLE_API_KEY')
-    url = f'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={truck_lat},{truck_long}&destinations={load_lat},{load_long}&key={api_key}'
+# def calculate_distance(truck_lat, truck_long, load_lat, load_long):
+#     api_key = os.getenv('GOOGLE_API_KEY')
+#     url = f'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={truck_lat},{truck_long}&destinations={load_lat},{load_long}&key={api_key}'
     
-    response = requests.get(url)
-    data = json.loads(response.text)
-    #print(data)
-    if 'rows' in data and 'elements' in data['rows'][0] and 'distance' in data['rows'][0]['elements'][0]:
-        return data['rows'][0]['elements'][0]['distance']['value']
-    else:
-        return None
+#     response = requests.get(url)
+#     data = json.loads(response.text)
+#     #print(data)
+#     if 'rows' in data and 'elements' in data['rows'][0] and 'distance' in data['rows'][0]['elements'][0]:
+#         return data['rows'][0]['elements'][0]['distance']['value']
+#     else:
+#         return None
 
-def notify_trucker(truck_id, load_id):
-    print(f'Notify truck {truck_id} about load {load_id}')
+# def notify_trucker(truck_id, load_id):
+#     print(f'Notify truck {truck_id} about load {load_id}')
 
 client = mqtt.Client(client_id="LoadBalancers01")
 client.username_pw_set("CodeJamUser", "123CodeJam")
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.connect("fortuitous-welder.cloudmqtt.com", 1883, 60)
-client.loop_forever()
+#client.loop_forever()
+# Run the loop for 12.5 seconds (1 hour simulation time)
+timeout = 12.5
+start_time = time.time()
+
+while time.time() - start_time < timeout:
+    client.loop()
+
+# Clean up and disconnect
+client.disconnect()
---
+++ backend/heuristic.py
@@ -0,0 +1,131 @@
+from datetime import datetime
+import math
+
+class Heuristic:
+    """
+    Heuristic class is used to initially find potential trucks for a load based on various attributes 
+    like profit, equipment type, trip length preference, and idle time.
+    """
+
+    def __init__(self, load, truck):
+        """
+        Initializes the Heuristic with load and truck data.
+        """
+        self.load = load
+        self.truck = truck
+
+    def get_score(self):
+        """Calculates a simple heuristic score for given truck."""
+        score = 0
+        score += self.calculate_profit_score(self.truck)
+        score += self.trip_length_preference_score(self.truck)
+        score += self.idle_time_score(self.truck)
+        return score
+
+    def calculate_profit_score(self, truck):
+        """Calculates a score based on estimated profit."""
+        # Profit calculation
+        distance_in_miles = ((Heuristic.bird_fly_distance(self.truck['positionLatitude'], self.truck['positionLongitude'], self.load['originLatitude'], self.load['originLongitude'])) * 0.000621371)
+        profit = self.load['price'] - (self.load['mileage'] * 1.38) - (distance_in_miles*1.38)
+        print("Profit:",profit)
+        print("Distance:",distance_in_miles)
+        return profit / 1000  # Scale down the profit for scoring
+
+    def trip_length_preference_score(self, truck):
+        """Scores based on trip length preference."""
+        preference = 1 if truck['nextTripLengthPreference'] == 'Long' else 0
+        load_trip_length = 1 if self.load['mileage'] >= 200 else 0
+        return 5 if preference == load_trip_length else 0
+
+    def idle_time_score(self, truck):
+        """Scores based on the difference in timestamp (idle time)."""
+        # Convert string timestamps to datetime objects
+        truck_timestamp = datetime.fromisoformat(truck['timestamp'])
+        load_timestamp = datetime.fromisoformat(self.load['timestamp'])
+
+        # Calculate the absolute time difference in hours
+        time_difference = abs((truck_timestamp - load_timestamp).total_seconds() / 3600)
+
+        # Assuming a maximum idle time threshold (e.g., 48 hours)
+        max_idle_threshold = 48
+        # Trucks with shorter idle times get higher scores
+        return max(0, max_idle_threshold - time_difference) / max_idle_threshold * 5
+    
+    @staticmethod
+    def bird_fly_distance(truck_lat, truck_long, load_lat, load_long):
+        # Radius of the Earth in meters
+        R = 6371000 
+
+        # Convert degrees to radians
+        truck_lat, truck_long, load_lat, load_long = map(math.radians, [truck_lat, truck_long, load_lat, load_long])
+
+        # Differences
+        diff_lat = load_lat - truck_lat
+        diff_long = load_long - truck_long
+
+        # Haversine formula
+        a = math.sin(diff_lat/2) ** 2 + math.cos(truck_lat) * math.cos(load_lat) * math.sin(diff_long/2) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+        # Distance in meters
+        distance = R * c
+
+        return distance
+
+
+#############################
+#         TESTING           #
+#############################
+
+# Load and truck data from algorithm.py
+load = {
+    'seq': 51,
+    'type': 'Load',
+    'timestamp': '2023-11-18T17:46:00', 
+    'loadId': 40022,
+    'originLatitude': 29.9561,
+    'originLongitude': -90.0773, 
+    'destinationLatitude': 33.6821,
+    'destinationLongitude': -84.1488, 
+    'equipmentType': 'Flatbed',
+    'price': 1000.0,
+    'mileage': 480.0
+}
+
+truck_list = [
+    {'seq': 1758, 'type': 'Truck', 'timestamp': '2023-11-18T17:46:07', 'truckId': 160, 'positionLatitude': 41.19511413574219, 'positionLongitude': -79.43486785888672, 'equipType': 'Van', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1759, 'type': 'Truck', 'timestamp': '2023-11-18T17:46:17', 'truckId': 427, 'positionLatitude': 33.104087829589844, 'positionLongitude': -83.81094360351562, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1760, 'type': 'Truck', 'timestamp': '2023-11-18T17:46:22', 'truckId': 215, 'positionLatitude': 35.41987609863281, 'positionLongitude': -86.014892578125, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1761, 'type': 'Truck', 'timestamp': '2023-11-18T17:46:38', 'truckId': 103, 'positionLatitude': 40.08848190307617, 'positionLongitude': -77.41670227050781, 'equipType': 'Van', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1762, 'type': 'Truck', 'timestamp': '2023-11-18T17:46:47', 'truckId': 276, 'positionLatitude': 42.1627311706543, 'positionLongitude': -83.242919921875, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1763, 'type': 'Truck', 'timestamp': '2023-11-18T17:47:08', 'truckId': 119, 'positionLatitude': 34.66741943359375, 'positionLongitude': -78.7313003540039, 'equipType': 'Van', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1764, 'type': 'Truck', 'timestamp': '2023-11-18T17:47:19', 'truckId': 290, 'positionLatitude': 36.65528106689453, 'positionLongitude': -78.1341323852539, 'equipType': 'Van', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1765, 'type': 'Truck', 'timestamp': '2023-11-18T17:47:41', 'truckId': 266, 'positionLatitude': 40.76347351074219, 'positionLongitude': -81.59420013427734, 'equipType': 'Van', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1766, 'type': 'Truck', 'timestamp': '2023-11-18T17:47:42', 'truckId': 403, 'positionLatitude': 36.32249450683594, 'positionLongitude': -86.70453643798828, 'equipType': 'Van', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1767, 'type': 'Truck', 'timestamp': '2023-11-18T17:47:54', 'truckId': 161, 'positionLatitude': 33.903926849365234, 'positionLongitude': -83.57975769042969, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1768, 'type': 'Truck', 'timestamp': '2023-11-18T17:48:47', 'truckId': 390, 'positionLatitude': 39.60343933105469, 'positionLongitude': -84.23631286621094, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1769, 'type': 'Truck', 'timestamp': '2023-11-18T17:48:50', 'truckId': 397, 'positionLatitude': 43.37735748291016, 'positionLongitude': -74.91069030761719, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1770, 'type': 'Truck', 'timestamp': '2023-11-18T17:48:53', 'truckId': 323, 'positionLatitude': 36.549991607666016, 'positionLongitude': -85.50529479980469, 'equipType': 'Van', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1771, 'type': 'Truck', 'timestamp': '2023-11-18T17:49:56', 'truckId': 181, 'positionLatitude': 40.64623260498047, 'positionLongitude': -74.59065246582031, 'equipType': 'Van', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1772, 'type': 'Truck', 'timestamp': '2023-11-18T17:49:59', 'truckId': 199, 'positionLatitude': 39.651126861572266, 'positionLongitude': -77.88816833496094, 'equipType': 'Van', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1773, 'type': 'Truck', 'timestamp': '2023-11-18T17:50:07', 'truckId': 338, 'positionLatitude': 36.16437530517578, 'positionLongitude': -84.07960510253906, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1774, 'type': 'Truck', 'timestamp': '2023-11-18T17:51:03', 'truckId': 342, 'positionLatitude': 40.71038436889648, 'positionLongitude': -74.16265106201172, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1775, 'type': 'Truck', 'timestamp': '2023-11-18T17:52:05', 'truckId': 134, 'positionLatitude': 39.89287185668945, 'positionLongitude': -86.05553436279297, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1776, 'type': 'Truck', 'timestamp': '2023-11-18T17:53:34', 'truckId': 232, 'positionLatitude': 34.792808532714844, 'positionLongitude': -85.00074005126953, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1777, 'type': 'Truck', 'timestamp': '2023-11-18T17:53:49', 'truckId': 234, 'positionLatitude': 43.75012969970703, 'positionLongitude': -79.41373443603516, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1778, 'type': 'Truck', 'timestamp': '2023-11-18T17:54:07', 'truckId': 418, 'positionLatitude': 35.47133255004883, 'positionLongitude': -78.37611389160156, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1779, 'type': 'Truck', 'timestamp': '2023-11-18T17:54:13', 'truckId': 236, 'positionLatitude': 35.253353118896484, 'positionLongitude': -82.29132843017578, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1780, 'type': 'Truck', 'timestamp': '2023-11-18T17:54:15', 'truckId': 129, 'positionLatitude': 32.41004943847656, 'positionLongitude': -83.31920623779297, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Short'},
+    {'seq': 1781, 'type': 'Truck', 'timestamp': '2023-11-18T17:54:28', 'truckId': 316, 'positionLatitude': 34.964813232421875, 'positionLongitude': -82.02007293701172, 'equipType': 'Reefer', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1782, 'type': 'Truck', 'timestamp': '2023-11-18T17:55:08', 'truckId': 159, 'positionLatitude': 34.650386810302734, 'positionLongitude': -82.57096099853516, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1783, 'type': 'Truck', 'timestamp': '2023-11-18T17:55:09', 'truckId': 137, 'positionLatitude': 40.59501647949219, 'positionLongitude': -74.23518371582031, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Long'},
+    {'seq': 1784, 'type': 'Truck', 'timestamp': '2023-11-18T17:55:21', 'truckId': 224, 'positionLatitude': 38.80051040649414, 'positionLongitude': -77.1159896850586, 'equipType': 'Flatbed', 'nextTripLengthPreference': 'Long'}
+]
+
+# Example of using the Heuristic class
+for truck in truck_list: 
+    heuristic = Heuristic(load, truck)
+    truck_score = heuristic.get_score()
+
+    print("Truck score:", truck_score)
+    print()
